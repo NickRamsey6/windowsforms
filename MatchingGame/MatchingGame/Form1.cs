e939a7275910512da8c0a807e2db563a26d8642f
NickRamsey6 <nramseysc@gmail.com>
2024-06-20 16:16:47 -0600

    Finish match game code

--- MatchingGame/MatchingGame/Form1.cs
+++ MatchingGame/MatchingGame/Form1.cs
@@ -55,6 +55,33 @@ namespace MatchingGame
             }
         }
 
+        ///<summary>
+        /// Check every icon to see if it is matched, by
+        /// comparing its foreground color to its background color.
+        /// If all of the icons are matched, the player wins
+        ///</summary>
+        private void CheckForWinner()
+        {
+            // Go through all of the labels in the TabelLayoutPanel,
+            // checking each one to see if its icon is matched
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+
+                if (iconLabel != null)
+                {
+                    if (iconLabel.ForeColor == iconLabel.BackColor)
+                        return;
+                }
+            }
+
+            // If the loop didn't return, it didn't find
+            // any unmatched icons
+            // That means the user won. Show a message and close the form
+            MessageBox.Show("You matched all the icons!", "Congratulations");
+            Close();
+        }
+
         public Form1()
         {
             InitializeComponent();
@@ -102,6 +129,19 @@ namespace MatchingGame
                 // Set its color to black
                 secondClicked = clickedLabel;
                 secondClicked.ForeColor = Color.Black;
+
+                // Check to see if the player won
+                CheckForWinner();
+
+                // If the player clicked two matching icons, keep them
+                // black and reset firstClicked and secondClicked
+                // so the player can click another icon
+                if (firstClicked.Text == secondClicked.Text)
+                {
+                    firstClicked = null;
+                    secondClicked = null;
+                    return;
+                }
 
                 // If the player gets this far, the player
                 // clicked two different icons, so start the
